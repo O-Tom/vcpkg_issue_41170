987d942c1cfb249b9ce6453a177069b0e97173d2
O-Tom <enrico.palazzo@libertysurf.fr>
2024-09-27 13:35:37 +0200

    chore(cmake): Using VCPKG_DEFAULT_TRIPLET as unique triplet env variable

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 export VCPKG_COMMIT_SHA=3508985146f1b1d248c67ead13f8f54be5b4f5da
-export VCPKG_TARGET_TRIPLET=x64-linux
+export VCPKG_DEFAULT_TRIPLET=x64-linux
 
 if [ ! -d "_vcpkg_" ]; then
   git clone https://github.com/microsoft/vcpkg.git ./_vcpkg_
